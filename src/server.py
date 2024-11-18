8b804cc727cb096c4010c677d5d67e8f7d1a4462
Владислав Кошелев <testvlad59@gmail.com>
2024-11-18 23:27:14 +0300

    код подправлен линтером flake8

--- src/server.py
+++ src/server.py
@@ -6,25 +6,19 @@ serverPort = 8080  # Порт для доступа по сети
 
 
 class MyServer(BaseHTTPRequestHandler):
-    """ 
-        Специальный класс, который отвечает за 
-        обработку входящих запросов от клиентов
-    """
+    """ Специальный класс, который отвечает за обработку входящих запросов от клиентов"""
     def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
-        # Открываем файл contacts.html и читаем его содержимое
         with open('C:/Users/user/PycharmProjects/Home_work_21_2_layouts/contacts.html', 'r', encoding='utf-8') as file:
             content = file.read()
 
-        self.send_response(200) # Отправка кода ответа
+        self.send_response(200)
         self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
         self.end_headers()  # Завершение формирования заголовков ответа
-        self.wfile.write(bytes(content, "utf-8"))  # Тело ответа
+        self.wfile.write(bytes(content, "utf-8"))
 
 
-if __name__ == "__main__":        
-    # Инициализация веб-сервера, который будет по заданным параметрах в сети
-    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
+if __name__ == "__main__":
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
